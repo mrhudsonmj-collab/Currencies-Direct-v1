10084247e9d890cf48873ac7c3e4fd336cbb5db6
mrhudsonmj-collab <mrhudsonmj@gmail.com>
2026-09-19 17:46:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-const C='wage-conversion-v3-live-comparison';
-const A=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
- if(e.request.url.includes('open.er-api.com')) return;
- e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
-});
+const C='wage-v4';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'])).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.url.includes('open.er-api.com'))return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
